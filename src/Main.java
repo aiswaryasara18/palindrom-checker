b4e9d2aa954195123c6b4bab29c110df1edc961a
Aiswarya Saravanan <aiswaryasara18@gmail.com>
2026-03-10 15:05:37 +0530

    UC4: Character Array Based Palindrome Check

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,34 @@
-class PalindromeUC3 {
+```java
+class PalindromeUC4 {
 
     public static void main(String[] args) {
 
         // Original string
-        String original = "level";
+        String word = "radar";
 
-        // Variable to store reversed string
-        String reversed = "";
+        // Convert string to character array
+        char[] chars = word.toCharArray();
 
-        // Reverse string using for loop
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+        // Two-pointer approach
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        // Compare original and reversed string
-        if (original.equals(reversed)) {
-            System.out.println(original + " is a Palindrome");
+        // Display result
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
         } else {
-            System.out.println(original + " is not a Palindrome");
+            System.out.println(word + " is not a Palindrome");
         }
-
     }
 }
+```
