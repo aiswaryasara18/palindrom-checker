deb56d10d8c6685faff9946c3562c38e4fc8a4cd
Aiswarya Saravanan <aiswaryasara18@gmail.com>
2026-03-10 15:45:04 +0530

    UC12: Strategy Pattern for Palindrome Algorithms (Advanced)

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,77 @@
-// PalindromeChecker class encapsulates the palindrome logic
-class PalindromeChecker {
+// Strategy Interface
+interface PalindromeStrategy {
+    boolean checkPalindrome(String text);
+}
 
-    // Method to check palindrome
+// Stack Strategy Implementation
+import java.util.Stack;
+
+class StackStrategy implements PalindromeStrategy {
+
     public boolean checkPalindrome(String text) {
+        Stack<Character> stack = new Stack<>();
 
-        char[] arr = text.toCharArray();
-        int start = 0;
-        int end = arr.length - 1;
+        for (char c : text.toCharArray()) {
+            stack.push(c);
+        }
 
-        while (start < end) {
-            if (arr[start] != arr[end]) {
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) != stack.pop()) {
                 return false;
             }
-            start++;
-            end--;
         }
         return true;
     }
 }
 
-// Main class
-public class PalindromeUC11 {
+// Deque Strategy Implementation
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean checkPalindrome(String text) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : text.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// Context Class
+class PalindromeContext {
+
+    private PalindromeStrategy strategy;
+
+    public PalindromeContext(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean execute(String text) {
+        return strategy.checkPalindrome(text);
+    }
+}
+
+// Main Class
+public class PalindromeUC12 {
 
     public static void main(String[] args) {
 
-        String word = "level";
+        String word = "madam";
 
-        // Create object of PalindromeChecker
-        PalindromeChecker checker = new PalindromeChecker();
+        // Choose strategy dynamically
+        PalindromeContext context = new PalindromeContext(new StackStrategy());
+        // PalindromeContext context = new PalindromeContext(new DequeStrategy());
 
-        boolean result = checker.checkPalindrome(word);
-
-        if (result) {
+        if (context.execute(word)) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is not a Palindrome");
