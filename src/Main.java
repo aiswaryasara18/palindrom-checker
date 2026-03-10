6dbd58441a6886ba10a1fc921206bf0ebd17463d
Aiswarya Saravanan <aiswaryasara18@gmail.com>
2026-03-10 14:47:16 +0530

    UC1: Add welcome message module

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+class PalindromeApp {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    public static void main(String[] args) {
+
+        // Application Entry & Welcome Message
+        System.out.println("=================================");
+        System.out.println("     PALINDROME CHECKER APP");
+        System.out.println("=================================");
+        System.out.println("Version: 1.0");
+        System.out.println("Welcome to the Palindrome Checker Application!");
+        System.out.println();
+
+        // Continue to next use case
+        System.out.println("Program is ready to check palindromes...");
     }
+
 }
